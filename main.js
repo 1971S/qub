dc64b26f6080e9a3cb476012e150b0ba6517dd31
Julián <jgpicatoste@gmail.com>
2019-02-27 16:18:03 +0100

    refactor
    modified the way fullscreen is achieved

--- main.js
+++ main.js
@@ -2,6 +2,9 @@ const app = new PIXI.Application({
   width: window.innerWidth,
   height: window.innerHeight,
   backgroundColor: 0x2c3e50,
+  resizeTo: document,
+  autoDensity: true,
+  resolution: devicePixelRatio
 });
 
 window.addEventListener("gamepadconnected", function(e) {
@@ -11,23 +14,44 @@ window.addEventListener("gamepadconnected", function(e) {
   }
 );
 
+function handleKeypress(event) {
+  if (event.keyCode === 13) {
+    toggleFullscreen();
+  }
+}
+
+
+function toggleFullscreen() {
+  let elem = document.querySelector("body");
+
+  elem.requestFullscreen = elem.requestFullscreen || elem.mozRequestFullscreen
+          || elem.msRequestFullscreen || elem.webkitRequestFullscreen;
+
+  if (!document.fullscreenElement) {
+    elem.requestFullscreen().then({}).catch(err => {
+      console.log(`Error attempting to enable full-screen mode: ${err.message} (${err.name})`);
+    });
+  } else {
+    if (document.exitFullscreen) {
+      document.exitFullscreen();
+    }
+  }
+}
+
 window.onload = () => {
 
+  window.addEventListener('keypress', handleKeypress, false)
+  
   app.resources = {};
   app.state = play;
   app.stats = new Stats();
-
+  
   const domElement = document.getElementById('body');
   app.stats.domElement.id = "stats";
   domElement.append(app.stats.domElement);
-
+  
   window.body.appendChild(app.view);
   app.ticker.add(delta => gameLoop(delta));
-
-  window.onresize = () => {
-    app.view.width = window.innerWidth;
-    app.view.height = window.innerHeight;
-  }
 
 }
 
